884d57220680b6807a28450d78f04c75ae25cece
U-ND3R <artiomchrisonopulo@gmail.com>
2025-11-08 17:35:48 +0200

    Signup page Frontend (removed comments)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center"> {/* Container fullscreen */}
-      <h1 className="text-5xl text-black font-extrabold m-10">Note Nest</h1> {/* Title */}
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl text-black font-extrabold m-10">Note Nest</h1>
       <Button variant={"ghost"} size={"lg"}>Click here to continue</Button>
     </div>
   );
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { 
+    Card, 
+    CardHeader, 
+    CardTitle,
+    CardDescription,
+    CardAction,
+    CardContent,
+    CardFooter
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+export default function Signup() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <Card className="w-full max-w-2xl">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login to your account</CardTitle>
+                    <CardDescription>Enter your email below to login to your account</CardDescription>
+                    <CardAction>
+                        <Button variant={"outline"}>Go to Sign up</Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <form action="submit">
+                        <div className="flex flex-col gap-5">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input type="email" id="email" placeholder="example@example.com" required />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="pswrd">Password</Label>
+                                <Input type="password" id="pswrd" placeholder="*****" required />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex gap-5">
+                    <Button type="submit" className="w-full">Login</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+}
